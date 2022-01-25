6a7300650c8270575df2ae33fb6681ec3dc9c2c3
Ing Davide Proietto <davide_ag@hotmail.it>
2022-01-25 19:39:05 +0100

    GPIO

--- gpio.f
+++ gpio.f
@@ -3,41 +3,43 @@
 \ Università degli Studi di Palermo 
 \ Davide Proietto matr. 0739290 LM Ingegneria Informatica, 21-22 
 
-\ Must be INCLUDEd after ans.f
+\ Includere dopo il flie ans.f
 
-\ GPIO definitions
+\ GPIO Mapping
 HEX
-FE000000 CONSTANT DEVBASE
-DEVBASE 200000 + CONSTANT GPFSEL0
-DEVBASE 200004 + CONSTANT GPFSEL1
-DEVBASE 200008 + CONSTANT GPFSEL2
-DEVBASE 200040 + CONSTANT GPEDS0
-DEVBASE 20001C + CONSTANT GPSET0
-DEVBASE 200028 + CONSTANT GPCLR0
-DEVBASE 200034 + CONSTANT GPLEV0
-DEVBASE 200058 + CONSTANT GPFEN0
+FE000000 CONSTANT BASE  \ Indirizzo base dei registri
+BASE 200000 + CONSTANT GPFSEL0  \ Spazio dei registri GPIO FE200000 
+BASE 200004 + CONSTANT GPFSEL1
+BASE 200008 + CONSTANT GPFSEL2
+BASE 200040 + CONSTANT GPEDS0
+BASE 20001C + CONSTANT GPSET0
+BASE 200028 + CONSTANT GPCLR0
+BASE 200034 + CONSTANT GPLEV0
+BASE 200058 + CONSTANT GPFEN0
 
-\ Applies Logical Left Shift of 1 bit on the given value
-\ Returns the shifted value
-\ Usage: 2 MASK
-  \ 2(BINARY 0010) -> 4(BINARY 0100)
+\ Applica lo spostamento logico sinistro di 1 bit sul valore dato
+\ e restituisce il valore spostato
+\ Utilizzo: 2 MASK
+   \ 2( BIN 0010 ) -> 4( BIN 0100 ) 
 : MASK 1 SWAP LSHIFT ;
 
-\ Sets the given GPIO pin to HIGH if configured as output
-\ Usage: 12 HIGH -> Sets the GPIO-18 to HIGH
+\ Imposta il pin GPIO specificato su HIGH se configurato come output
+\ Utilizzo: 12 ( pin fisico ) HIGH -> Imposta il GPIO-18 su HIGH 
 : HIGH 
   MASK GPSET0 ! ;
 
-\ Clears the given GPIO pin if configured as output
-\ Usage: 12 LOW -> Clears the GPIO-18
+\ Resetta il pin GPIO specificato se configurato come output
+\ Utilizzo: 12 ( pin fisico ) LOW -> Resetta il GPIO-18 
 : LOW 
   MASK GPCLR0 ! ;
 
-\ Tests the actual value of GPIO pins 0..31
-\ 0 -> GPIO pin n is low
-\ 1 -> GPIO pin n is high
-\ Usage: 12 TPIN (Test GPIO-18)
+\ Verifica il valore effettivo dei pin GPIO 0..31
+\ 0 -> Il pin GPIO n è LOW
+\ 1 -> Il pin GPIO n è HIGH
+\ Utilizzo: 12 TPIN (Test GPIO-18) 
 : TPIN GPLEV0 @ SWAP RSHIFT 1 AND ;
 
+\ Crea un tempo di attesa in millisecondi
+\ Utilizzo: 1000 DELAY
 : DELAY 
   BEGIN 1 - DUP 0 = UNTIL DROP ;
