b41839373a62a2df4449ad62b3eac37b51ec1d8f
Ing Davide Proietto <davide_ag@hotmail.it>
2022-03-01 10:27:55 +0100

    Aggiornamenti

--- ans.f
+++ ans.f
@@ -1,6 +1,6 @@
 \ Embedded Systems - Sistemi Embedded - 17873
 \ some dictionary definitions
-\ from  pijFORTHos and prof. D. Peri
+\ from  pijFORTHos
 \ modificated by Davide Proietto matr. 0739290 LM Ingegneria Informatica, 21-22
 
 \ '\n'	newline character (10)
@@ -8,27 +8,37 @@
 \ BL	blank character (32)
 : BL 32 ;
 
+\ STANDARD CHAR
 : ':' [ CHAR : ] LITERAL ;
 : ';' [ CHAR ; ] LITERAL ;
 : '(' [ CHAR ( ] LITERAL ;
 : ')' [ CHAR ) ] LITERAL ;
 : '"' [ CHAR " ] LITERAL ;
 : '.' [ CHAR . ] LITERAL ;
+: 'A' [ CHAR A ] LITERAL ;
+: '0' [ CHAR 0 ] LITERAL ;
+: '-' [ CHAR - ] LITERAL ;
 
 \ ?IMMEDIATE	( entry -- p )	get IMMEDIATE flag from dictionary entry
 \ ( comment text ) 	( -- )	comment inside definition
-\ SPACES	( n -- )	print n spaces
-\ WITHIN	( a b c -- p )	where p = ((a >= b) && (a < c))
-\ ALIGNED	( addr -- addr' )	round addr up to next 4-byte boundary
-\ ALIGN	( -- )	align the HERE pointer
-\ C,	( c -- )	write a byte from the stack at HERE
-\ S" string"	( -- addr len )	create a string value
 : ( IMMEDIATE 1 BEGIN KEY DUP '(' = IF DROP 1+ ELSE ')' = IF 1- THEN THEN DUP 0= UNTIL DROP ;
+
+\ SPACES	( n -- )	print n spaces
 : SPACES BEGIN DUP 0> WHILE SPACE 1- REPEAT DROP ;
+
+\ WITHIN	( a b c -- p )	where p = ((a >= b) && (a < c))
 : WITHIN -ROT OVER <= IF > IF TRUE ELSE FALSE THEN ELSE 2DROP FALSE THEN ;
+
+\ ALIGNED	( addr -- addr' )	round addr up to next 4-byte boundary
 : ALIGNED 3 + 3 INVERT AND ;
+
+\ ALIGN	( -- )	align the HERE pointer
 : ALIGN HERE @ ALIGNED HERE ! ;
+
+\ C,	( c -- )	write a byte from the stack at HERE
 : C, HERE @ C! 1 HERE +! ;
+
+\ S" string"	( -- addr len )	create a string value
 : S" IMMEDIATE ( -- addr len )
     STATE @ IF
         ' LITS , HERE @ 0 ,
@@ -43,7 +53,7 @@
     THEN
 ;
 
-\ ." string"	( -- )	print string
+\ ." STRING"	( -- )	print string
 : ." IMMEDIATE ( -- )
     STATE @ IF
         [COMPILE] S" ' TELL ,
@@ -52,6 +62,43 @@
     THEN
 ;
 
+: DICT WORD FIND ;
+: VALUE ( n -- ) WORD CREATE DOCOL , ' LIT , , ' EXIT , ;
+: TO IMMEDIATE ( n -- )
+        DICT >DFA 4+
+	STATE @ IF ' LIT , , ' ! , ELSE ! THEN
+;
+: +TO IMMEDIATE
+        DICT >DFA 4+
+	STATE @ IF ' LIT , , ' +! , ELSE +! THEN
+;
+: ID. 4+ COUNT F_LENMASK AND BEGIN DUP 0> WHILE SWAP COUNT EMIT SWAP 1- REPEAT 2DROP ;
+: ?HIDDEN 4+ C@ F_HIDDEN AND ;
+: ?IMMEDIATE 4+ C@ F_IMMED AND ;
+: WORDS LATEST @ BEGIN ?DUP WHILE DUP ?HIDDEN NOT IF DUP ID. SPACE THEN @ REPEAT CR ;
+: FORGET DICT DUP @ LATEST ! HERE ! ;
+: CFA> LATEST @ BEGIN ?DUP WHILE 2DUP SWAP < IF NIP EXIT THEN @ REPEAT DROP 0 ;
+: SEE
+	DICT HERE @ LATEST @
+	BEGIN 2 PICK OVER <> WHILE NIP DUP @ REPEAT
+	DROP SWAP ':' EMIT SPACE DUP ID. SPACE
+	DUP ?IMMEDIATE IF ." IMMEDIATE " THEN
+	>DFA BEGIN 2DUP
+        > WHILE DUP @ CASE
+		' LIT OF 4 + DUP @ . ENDOF
+		' LITS OF [ CHAR S ] LITERAL EMIT '"' EMIT SPACE
+			4 + DUP @ SWAP 4 + SWAP 2DUP TELL '"' EMIT SPACE + ALIGNED 4 -
+		ENDOF
+		' 0BRANCH OF ." 0BRANCH ( " 4 + DUP @ . ." ) " ENDOF
+		' BRANCH OF ." BRANCH ( " 4 + DUP @ . ." ) " ENDOF
+		' ' OF [ CHAR ' ] LITERAL EMIT SPACE 4 + DUP @ CFA> ID. SPACE ENDOF
+		' EXIT OF 2DUP 4 + <> IF ." EXIT " THEN ENDOF
+		DUP CFA> ID. SPACE
+	ENDCASE 4 + REPEAT
+	';' EMIT CR 2DROP
+;
+: :NONAME 0 0 CREATE HERE @ DOCOL , ] ;
+: ['] IMMEDIATE ' LIT , ;
 : EXCEPTION-MARKER RDROP 0 ;
 : CATCH ( xt -- exn? ) DSP@ 4+ >R ' EXCEPTION-MARKER 4+ >R EXECUTE ;
 : THROW ( n -- ) ?DUP IF
@@ -92,6 +139,20 @@
 : DOES>COMP   ['] LIT , HERE 3 CELLS + , ['] (DODOES-COMP) , ['] EXIT , ;
 : DOES>INT   (DODOES-INT) LATEST @ HIDDEN ] ;
 : DOES>   STATE @ 0= IF DOES>INT ELSE DOES>COMP THEN ; IMMEDIATE
+
+: PRINT-STACK-TRACE
+	RSP@ BEGIN DUP R0 4-
+        < WHILE DUP @ CASE
+		' EXCEPTION-MARKER 4+ OF ." CATCH ( DSP=" 4+ DUP @ U. ." ) " ENDOF
+		DUP CFA> ?DUP IF 2DUP ID. [ CHAR + ] LITERAL EMIT SWAP >DFA 4+ - . THEN
+	ENDCASE 4+ REPEAT DROP CR
+;
+: BINARY ( -- ) 2 BASE ! ;
+: OCTAL ( -- ) 8 BASE ! ;
+: 2# BASE @ 2 BASE ! WORD NUMBER DROP SWAP BASE ! ;
+: 8# BASE @ 8 BASE ! WORD NUMBER DROP SWAP BASE ! ;
+: # ( b -- n ) BASE @ SWAP BASE ! WORD NUMBER DROP SWAP BASE ! ;
+
 : UNUSED ( -- n ) PAD HERE @ - 4/ ;
 
 \ Control Structures
@@ -107,8 +168,6 @@
 \ BEGIN p WHILE loop-part REPEAT	( -- )	pre-test loop
 \ CASE cases... default ENDCASE	( selector -- )	select case based on selector value
 \ value OF case-body ENDOF	( -- )	execute case-body if (selector == value)
-
-DROP
 
 \ Ritorna informazioni sull'autore delle modifiche
 : AUTHOR
